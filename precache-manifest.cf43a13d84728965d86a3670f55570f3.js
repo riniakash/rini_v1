124ba583ae4cd18b5c33938611abdb2754f19ee6
AkSingh17 <akashsinghkumar8@gmail.com>
2021-06-21 06:35:43 +0530

    Updates

---
+++ precache-manifest.cf43a13d84728965d86a3670f55570f3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b696e18a719df132ad8e3eaf4ed1102b",
+    "url": "/riniakash/rini_v1.git/index.html"
+  },
+  {
+    "revision": "e81495b4f352ed3c35e5",
+    "url": "/riniakash/rini_v1.git/static/css/2.22ca87da.chunk.css"
+  },
+  {
+    "revision": "831dad3864eefeae3324",
+    "url": "/riniakash/rini_v1.git/static/css/main.3c18c749.chunk.css"
+  },
+  {
+    "revision": "e81495b4f352ed3c35e5",
+    "url": "/riniakash/rini_v1.git/static/js/2.6d24d321.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/riniakash/rini_v1.git/static/js/2.6d24d321.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "831dad3864eefeae3324",
+    "url": "/riniakash/rini_v1.git/static/js/main.f65ec10e.chunk.js"
+  },
+  {
+    "revision": "0e209a7f860684daff45",
+    "url": "/riniakash/rini_v1.git/static/js/runtime-main.ad32c2f6.js"
+  },
+  {
+    "revision": "cd1d1c978d0030f14f1d8a7690eb8398",
+    "url": "/riniakash/rini_v1.git/static/media/img-1.cd1d1c97.jpg"
+  },
+  {
+    "revision": "4143d240af520e4a8b565060c6e6277b",
+    "url": "/riniakash/rini_v1.git/static/media/img-2.4143d240.jpg"
+  },
+  {
+    "revision": "bd11375f25b4d7b599e2065c9e916507",
+    "url": "/riniakash/rini_v1.git/static/media/img-6.bd11375f.jpg"
+  },
+  {
+    "revision": "66001cf811f9f101c450edb126e29c63",
+    "url": "/riniakash/rini_v1.git/static/media/img-7.66001cf8.jpg"
+  },
+  {
+    "revision": "a4b94b49f8c889bb1b7b37f95185c66b",
+    "url": "/riniakash/rini_v1.git/static/media/star-2.a4b94b49.svg"
+  }
+]);
